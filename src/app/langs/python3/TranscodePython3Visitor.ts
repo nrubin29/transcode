99c9b161b0536636d456ff5de6ac7c17cda72c7b
hvl5451 <hvl5451@psu.edu>
2019-11-02 23:08:23 -0400

    Implemented assignment and logical ops

--- src/app/langs/python3/TranscodePython3Visitor.ts
+++ src/app/langs/python3/TranscodePython3Visitor.ts
@@ -1,12 +1,26 @@
 import {Python3Visitor} from '../../../antlr/python3/Python3Visitor';
-import {ArithmeticNode, BinaryLogicalOperation, FunctionCallNode, Node, RootNode, UnaryLogicalOperation} from '../ast';
+import {
+  ArithmeticNode,
+  BinaryLogicalOperation,
+  FunctionCallNode,
+  Node,
+  RootNode,
+  UnaryLogicalOperation,
+  ExpressionNode,
+  AssignmentNode, AtomNode, BinaryLogicalNode, UnaryLogicalNode
+} from '../ast';
 import {
   ArglistContext,
   Arith_exprContext,
   AtomContext,
   File_inputContext,
   PowerContext,
-  TrailerContext
+  TrailerContext,
+  Expr_stmtContext,
+  Testlist_star_exprContext,
+  Or_testContext,
+  And_testContext,
+  Not_testContext,
 } from '../../../antlr/python3/Python3Parser';
 import {TranscodeVisitor} from '../transcode-visitor';
 import {ParseTree} from 'antlr4ts/tree';
@@ -56,4 +70,42 @@ export class TranscodePython3Visitor extends TranscodeVisitor implements Python3
       case 'not': return UnaryLogicalOperation.NOT;
     }
   }
+
+
+  visitExpr_stmt(ctx: Expr_stmtContext) {
+    if (ctx.childCount === 3) {
+      // Should be of type AtomNode
+      const left = this.visit(ctx.testlist_star_expr(0));
+      // Should be of type ExpressionNode
+      const right = this.visit(ctx.testlist_star_expr(1));
+      return new AssignmentNode(left as AtomNode, right);
+    }
+  }
+
+  visitOr_test(ctx: Or_testContext) {
+      if (ctx.childCount === 3) {
+      const left = this.visit(ctx.and_test(0));
+      const right = this.visit(ctx.and_test(1));
+      const operator = this.visitBinaryLogicalOperation(ctx.getChild(1));
+      return new BinaryLogicalNode(left, right, operator);
+    }
+  }
+
+  visitAnd_test(ctx: And_testContext) {
+    if (ctx.childCount === 3) {
+      const left = this.visit(ctx.not_test(0));
+      const right = this.visit(ctx.not_test(1));
+      const operator = this.visitBinaryLogicalOperation(ctx.getChild(1));
+      return new BinaryLogicalNode(left, right, operator);
+    }
+  }
+
+  visitNot_test(ctx: Not_testContext) {
+    if (ctx.childCount === 2) {
+      const right = this.visit(ctx.not_test());
+      const operator = this.visitUnaryLogicalOperation(ctx.getChild(0));
+      return new UnaryLogicalNode(right, operator);
+    }
+  }
+
 }
